bc10bb0c630264e8a09ae504ec9fe4ba71a90317
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-08-06 11:09:22 +0530

    bootstrap added

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,15 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
-
-  console.log('testing')
   const location = search.value
 
   fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
-      debugger
       if (data.error) {
         messageOne.textContent = data.error
       } else {
-        messageOne.textContent = data.temp
-        messageTwo.textContent = data.address
+        messageOne.textContent = 'temparature is ' + data.temp
+        messageTwo.textContent = 'your area ' + data.address
       }
     })
   })
